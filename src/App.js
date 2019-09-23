3470c5f42684aa99e66d31ba1a9674e77daefa93
George <gwong323@yahoo.com>
2019-09-23 23:52:29 +0100

    basic form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = { startStation: "", endStation: "" };
 
   whatever = () => {};
 
   render() {
     return (
       <div className="card">
-        <div> </div>
+        <div> h1 TFL calculator</div>
+        <div>
+          <form action="">
+            <input type="text" />
+            select start station
+            <input
+              onChange={event => {
+                this.setState({ startStation: event.target.value });
+              }}
+              name="station"
+              placeholer="station"
+              type="text"
+            />
+            select end station
+            <input
+              onChange={event => {
+                this.setState({ endStation: event.target.value });
+              }}
+              name="station"
+              placeholer="station"
+              type="text"
+            />
+          </form>
+        </div>
       </div>
     );
   }
