128910f72e0ed4f7c798f19c47e5542b86c7ea16
George <gwong323@yahoo.com>
2019-11-24 22:45:32 +0000

    results minor changes

--- src/Results.js
+++ src/Results.js
@@ -29,20 +29,22 @@ class Results extends Component {
     return (
       <div className="results">
         <h1>Results</h1>
-        <p>Zones Travelled {this.props.zones}</p>
+        <br />
+        <h4>Zones Travelled {this.props.zones}</h4>
+        <br />
         <div className="resultsLeft">
           <p>contactless</p>
-          <p>weekly {this.props.week}</p>
-          <p>monthly {this.props.month}</p>
-          <p>6 month {this.props.halfYear}</p>
-          <p>year {this.props.year}</p>
+          <p>weekly £{this.props.week}</p>
+          <p>monthly £{this.props.month}</p>
+          <p>6 month £{this.props.halfYear}</p>
+          <p>year £{this.props.year}</p>
         </div>
         <div className="resultsRight">
           <p>travel card </p>
-          <p>weekly {this.props.weekCard}</p>
-          <p>monthly {this.props.monthCard}</p>
-          <p>6 month {this.props.halfYearCard}</p>
-          <p>year {this.props.yearCard}</p>
+          <p>weekly £{this.props.weekCard}</p>
+          <p>monthly £{this.props.monthCard}</p>
+          <p>6 month £{this.props.halfYearCard}</p>
+          <p>year £{this.props.yearCard}</p>
         </div>
         <div className="resultsButtons">
           <button onClick={() => this.props.formReturn()}>Edit</button>
