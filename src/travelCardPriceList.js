220f67d533c6d34846fbebd8dce8721ad19f8d9a
George <gwong323@yahoo.com>
2019-12-07 19:37:46 +0000

    start travel card price list

---
+++ src/travelCardPriceList.js
@@ -0,0 +1,4 @@
+var travelCard = [
+  { zone: "1-1", peak: 7, offPeak: 7, week: 35.1, month: 134.8, year: 1404 },
+  { zone: "1-2", peak: 7.0, offPeak: 7.0, week: 35.1, month: 134.8, year: 1404 }
+];
