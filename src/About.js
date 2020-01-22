940233c9825894c51aed7ea2b2edd866d69368c9
keoki33 <gwong323@yahoo.com>
2020-01-22 13:38:05 +0000

    updated about

--- src/About.js
+++ src/About.js
@@ -12,8 +12,24 @@ class About extends Component {
           commute and trying to figure out if it was worth buying a Travelcard I
           created a website to let you easily make that determination.
         </p>
+        <br />
         <p>
-          Simple assumes:
+          Choose your start and end stations for morning and afternoon commute.{" "}
+          <br />
+          Peak fares - Monday to Friday (not on public holidays) journeys
+          starting between 06:30 and 09:30, and between 16:00 and 19:00.
+          Off-peak fares - at all other times and if you travel from a station
+          outside Zone 1 to a station in Zone 1 between 16:00 and 19:00, Monday
+          to Friday. <br />
+          Select number of bus trips, each one allows unlimited tap on/off bus
+          and trams for one hour. Only select more than one if you need to
+          switch buses after one hour. <br />
+        </p>
+        Use simple if you travel same commute Mon-Fri and want basic average
+        commute cost. Use custom to select commute for every day including
+        weekends. <br />
+        <p>
+          Simple calculation:
           <br />
           <br />
           Week commute Monday to Friday with no commute on weekends. <br />
@@ -21,9 +37,12 @@ class About extends Component {
           Half Year is 126 working days <br />
           Year is 253 working days <br />
           <br />
-          Complex assumes: <br />
+          Complex calculation: <br />
           <br />
-          Same as simple plus 104 weekend days. <br />
+          Does not subtract any holidays. <br />
+          Month is averaged as 4.3 weeks. <br />
+          Half Year is 126 weeks. <br />
+          Year is 52 weeks. <br />
           <br />
           <br />
           All fares assume you are not tapping out or switching trains to avoid
@@ -31,7 +50,6 @@ class About extends Component {
           trip planner. If fare shows £0, TFL could not find a tube journey
           between the two stations.
         </p>
-
         <p>
           This website is in no way associated with TFL. Any errors in
           calculation and/or costs please report to
